ec66e895b16208d75296322b4bacfb5e62149260
VArtzy <farrelnikoson@gmail.com>
2023-06-28 19:39:01 +0700

    s

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -19,7 +19,7 @@ interface PlayerContentProps {
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
     const player = usePlayer()
-    const [volume, setVolume] = useLocalStorage("volume", 1)
+    const [volume, setVolume] = useState(1)
     const [isPlaying, setIsPlaying] = useState(false)
 
     const Icon = isPlaying ? BsPauseFill : BsPlayFill
